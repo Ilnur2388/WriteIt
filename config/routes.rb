b0a048291240f79583df19b7a51db0a288829139
Ilnur Nurumov <ilnur.nurumov@gmail.com>
2024-05-27 10:16:45 +0300

    Change routes and schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  root 'posts#index'
+  get 'search', to: "search#index"
+    resource :session, only: %i[new create destroy]
+
+    resources :users, only: %i[new create edit update]
+    resources :posts, except: :show
+
+    namespace :admin do
+        resources :users, only: %i[index create edit update destroy]
+        resources :posts, only: %i[index edit update destroy]
+    end
+
+    root 'pages#index'
 end
