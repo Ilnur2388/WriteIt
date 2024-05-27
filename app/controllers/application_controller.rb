aa1c1ce739f2b01187e3f6aa15c8e3ce58835062
Ilnur Nurumov <ilnur.nurumov@gmail.com>
2024-05-27 10:15:24 +0300

    Change controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_query
+
+  include Authentication
+  include Authorization
+
+  def set_query
+    @query = Post.ransack(params[:q])
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,67 @@
 class PostsController < ApplicationController
+  before_action :require_current_user
+  before_action :set_post!, only: %i[destroy edit update]
+  # before_action :send_post!, only: :create
+  before_action :authorize_post!
+
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build post_params
+    @post.state = params[:draft] ? :draft : :review
+    if @post.save
+      flash[:success] = 'Пост создан успешно!'
+      redirect_to posts_path
+    else
+      flash[:warning] = 'Не удается создать пост!'
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update post_params
+      @post.images.attach(params[:post][:images])
+      flash[:success] = 'Пост успешно отредактирован!'
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @post.state == 'draft'
+      @post.destroy
+      flash[:success] = 'Пост удален!'
+    else
+      flash[:warning] = 'Пост не может быть удален'
+    end
+    redirect_to posts_path
+  end
+
+  private
+
+  def set_post!
+    @post = Post.find params[:id]
+  end
+
+  # def send_post!
+  #   @post.city == current_user.city
+  # end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :state, :city, images: [])
+  end
+
+  def authorize_post!
+    authorize(@post || Post)
   end
 end
